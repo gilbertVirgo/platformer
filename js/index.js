c417d23cb31c0d5735ebc65655b01eaea71e115a
gilbertVirgo <gilbertjvirgo@gmail.com>
2020-01-18 11:32:02 +0000

    With Seth IP

--- js/index.js
+++ js/index.js
@@ -3,8 +3,7 @@ import Client from "./client/index.js";
 import { Socket } from "./handler/index.js";
 
 const init = async () => {
-	//const socket = new Socket("ws://192.168.1.113:1234/");
-	const socket = new Socket("ws://0.0.0.0:1234");
+	const socket = new Socket("ws://192.168.1.113:1234/");
 
 	socket.on("error", console.error);
 	socket.on("join", ({ entities }) => {
